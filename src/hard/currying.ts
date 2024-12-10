4272cb30ed9c7e411c1243f83e89a5b7cbf70e85
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-12-10 22:37:32 +0100

    chore: removed comments

--- src/hard/currying.ts
+++ src/hard/currying.ts
@@ -8,9 +8,7 @@ declare function Currying<T, K extends any[], R = undefined>(
   fn: (arg0: R, ...arg1: K) => T
 ): Currying<T, [K, R]>
 
-// declare function Currying<T, K extends any[], R = undefined>(fn: (arg0: R, ...arg1: K) => T): R extends undefined ? () => T : K extends [] ? (arg: R) => T : K extends [infer F, ...infer Z] ? Curryings<T, Z, F> : never
-
-const curried1 = Currying((a: string, b: number, c: boolean) => true)
+const curried1 = Currying((a: string, b: boolean, c: boolean) => true)
 const curried2 = Currying(
   (
     a: string,
---
+++ src/hard/union-to-intersection.ts
@@ -0,0 +1,18 @@
+// ### Question
+
+// Implement the advanced util type `UnionToIntersection<U>` which combines the union `U` into an intersection. For example:
+
+type UnionToIntersection<U, V = U> = U extends U
+  ? U &
+      (Exclude<V, U> extends never
+        ? unknown
+        : UnionToIntersection<Exclude<V, U>>)
+  : unknown
+
+type Union1 = UnionToIntersection<'foo' | 42 | true>
+
+type Union2 = UnionToIntersection<1 | 2 | 3>
+
+type Union3 = UnionToIntersection<(() => 'foo') | ((i: 42) => true)>
+
+type Union4 = UnionToIntersection<boolean | true>
