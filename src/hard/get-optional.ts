890008bd1b789a1703a6da62950ef67f3f047bb9
olamilekan@root <olorunnisholaolamilekan@gmail.com>
2024-12-17 09:57:21 +0100

    feat: get optional

---
+++ src/hard/get-optional.ts
@@ -0,0 +1,12 @@
+type GetOptional<T> = {
+  [K in keyof T as { [O in keyof T as O extends K ? never : O]: T[O] } extends T
+    ? K
+    : never]: T[K]
+}
+
+type optional1 = GetOptional<{ foo: number; bar?: string }> // expected to be { foo: number }
+type optional2 = GetOptional<{ foo: undefined; bar?: undefined }> // expected to be { foo: undefined }
+
+type check = { foo: number; bar?: string } extends { foo: number }
+  ? true
+  : false
